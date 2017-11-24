344b4edc20bf1ca2c5295b1374bba28b3de749b8
Philipp Tornow <philipp_oeffentlich@hotmail.de>
2017-11-24 18:51:11 +0100

    add comments

--- src/Aufgabe_2_3/SchiffeVersenken.java
+++ src/Aufgabe_2_3/SchiffeVersenken.java
@@ -11,18 +11,51 @@ public class SchiffeVersenken {
 	public static void main(String[] args) {
 		
 		initPlayArea();
+		play();			
+	}
+	
+	public static void initPlayArea()
+	{
+		
+		//init playarea
+		playarea = new boolean[10][10];
+		
+		//set all elements to false
+		for (int x = 0; x<10; x++){			
+			for (int y = 0; y<10; y++)
+			{
+				playarea[x][y]=false;
+			}				
+		}
+		
+		//create some test-ships		
+		playarea[2][2] = true;
+		playarea[2][3] = true;
+		playarea[5][4] = true;
+		playarea[4][4] = true;
+		playarea[8][9] = true;
+		playarea[7][9] = true;
+		playarea[9][1] = true;
+		
+		//set number of ships as for counting remaining ships
+		gameOver = 7;
+	}
+	
+	public static void play(){
+		
 		System.out.println("Try to hit me: ");
 		
 		sc = new Scanner(System.in);
-		
 		while (gameOver > 0){
 			
+			//input
 			System.out.println("x coordinate");
 			int inX = sc.nextInt();
 		
 			System.out.println("y coordinate");
 			int inY = sc.nextInt();
 		
+			//logic
 			if (hit(inX,inY)){
 				System.out.println("Eyyyy, you hit me!");
 				
@@ -39,42 +72,12 @@ public class SchiffeVersenken {
 		
 		System.out.println("GAME OVER");
 		sc.close();
-				
+		
 	}
 	
-	
-	public static void initPlayArea()
-	{
-		
-		//init playarea
-		playarea = new boolean[10][10];
-		
-		//set all elements to false
-		for (int x = 0; x<10; x++){			
-			for (int y = 0; y<10; y++)
-			{
-				playarea[x][y]=false;
-			}				
-		}
-		
-		playarea[2][2] = true;
-		playarea[2][3] = true;
-		playarea[5][4] = true;
-		playarea[4][4] = true;
-		playarea[8][9] = true;
-		playarea[7][9] = true;
-		playarea[9][1] = true;
-		
-		gameOver = 7;
-
-	}
-	
+	//check for hit
 	public static boolean hit(int X,int Y){		
-		
+				
 		return playarea[X][Y];
 	}
-	
-	
-	
-
 }
